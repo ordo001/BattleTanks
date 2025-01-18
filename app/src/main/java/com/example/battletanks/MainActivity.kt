547c16c475f543a25168cee9c5da0cc4940670fd
ordo001 <113034616+ordo001@users.noreply.github.com>
2025-01-18 11:14:34 +0300

    v2.0

--- app/src/main/java/com/example/battletanks/MainActivity.kt
+++ app/src/main/java/com/example/battletanks/MainActivity.kt
@@ -1,11 +1,98 @@
 package com.example.battletanks
 
+import android.icu.number.NumberRangeFormatter.RangeIdentityFallback
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_DPAD_UP
+import android.view.KeyEvent.KEYCODE_DPAD_DOWN
+import android.view.KeyEvent.KEYCODE_DPAD_LEFT
+import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
+import com.example.battletanks.Direction.UP
+import com.example.battletanks.Direction.LEFT
+import com.example.battletanks.Direction.RIGHT
+import com.example.battletanks.Direction.DOWN
+import com.example.battletanks.databinding.ActivityMainBinding
+
+const val CELL_SIZE = 50
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val gridDrawer by lazy {
+        GridDrawer(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Menu"
+    }
+
+    //Наполнение меню
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings, menu)
+        return true
+    }
+
+    //Отслеживание какое меню выбрали
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.menu_settings ->{
+                gridDrawer.drawGrid()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when(keyCode){
+            KEYCODE_DPAD_UP-> move(UP)
+            KEYCODE_DPAD_DOWN-> move(DOWN)
+            KEYCODE_DPAD_LEFT-> move(LEFT)
+            KEYCODE_DPAD_RIGHT-> move(RIGHT)
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun move(direction: Direction){
+        when(direction){
+            UP->{
+                binding.myTank.rotation = 0f
+                if (binding.myTank.marginTop > 0 ) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+                }
+            }
+            DOWN->{
+                binding.myTank.rotation = 180f
+                if ( binding.myTank.marginTop + binding.myTank.height < binding.container.height / CELL_SIZE * CELL_SIZE ) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+                }
+            }
+            LEFT->{
+                binding.myTank.rotation = 270f
+                if ( binding.myTank.marginLeft > 0 ) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= CELL_SIZE
+                }
+            }
+            RIGHT->{
+                binding.myTank.rotation = 90f
+                if ( binding.myTank.marginLeft + binding.myTank.width < binding.container.width / CELL_SIZE * CELL_SIZE) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+                }
+            }
+
+        }
+        binding.container.removeView(binding.myTank)
+        binding.container.addView(binding.myTank)
     }
 }
