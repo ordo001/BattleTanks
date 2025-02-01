e9c365beb36fcc56f90245b8a584bc13931b66d5
ordo001 <113034616+ordo001@users.noreply.github.com>
2025-02-01 10:19:35 +0300

    v.6

---
+++ app/src/main/java/com/example/battletanks/enums/Direction.kt
@@ -0,0 +1,8 @@
+package com.example.battletanks.enums
+
+enum class Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT,
+}
---
+++ app/src/main/java/com/example/battletanks/models/Coordinate.kt
@@ -0,0 +1,6 @@
+package com.example.battletanks.models
+
+data class Coordinate(
+    val top: Int,
+    val left: Int
+)
